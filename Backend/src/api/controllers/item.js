cb88a3e5661ed3f69f64d16b991071aa47961936
Nithin-Reddy-02 <jonnynithin7@gmail.com>
2021-09-23 10:51:50 +0530

    added getItems

--- Backend/src/api/controllers/item.js
+++ Backend/src/api/controllers/item.js
@@ -1,24 +1,38 @@
-import Item from '../models/item';
+import Item from '../models/item'
 
-
-
-export async function filterStringsToArrays(req,res){
-    await Item.find({}).then(async oItems=>{
-        await oItems.forEach(oItem=>{
-            oItem.categories = oItem.category.split(",")
-            oItem.categories = oItem.categories.map(oString=>oString.replace(/\s+/g, ''));
-            oItem.save((err)=>{
-                if(err){
-                    console.log(err);
-                }else{
-                    console.log('Saved Successfully');
-                }
-            })
-        })
+export async function filterStringsToArrays(req, res) {
+  await Item.find({}).then(async (oItems) => {
+    await oItems.forEach((oItem) => {
+      oItem.categories = oItem.category.split(',')
+      oItem.categories = oItem.categories.map((oString) =>
+        oString.replace(/\s+/g, '')
+      )
+      oItem.save((err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('Saved Successfully')
+        }
+      })
     })
-    return res.status(200).send({message : 'Success'});
+  })
+  return res.status(200).send({ message: 'Success' })
 }
 
+export async function getItems(req, res) {
+  let text = req.query.search
+  console.log(text)
+  const namePromise = Item.find({ name: text })
+  const categoryPromise = Item.find({ categories: text })
+  Promise.all([namePromise, categoryPromise]).then((data, error) => {
+    if (error) {
+      res.status(400)
+    } else {
+      console.log(data)
+      res.status(200).json({ byText: data[0], byCategory: data[1] })
+    }
+  })
+}
 
 /*
     let text = req.query.search;
@@ -28,7 +42,6 @@ export async function filterStringsToArrays(req,res){
         [[{},{}],[{},{}]]
     })
 */
-
 
 /*
     {
--- Backend/src/routes/api.js
+++ Backend/src/routes/api.js
@@ -1,11 +1,11 @@
-import * as userApi from '../api/controllers/user';
+import * as userApi from '../api/controllers/user'
 import * as itemApi from '../api/controllers/item'
 
-module.exports = (router)=>{
-    router.get('/',(req,res)=>{
-        res.status(200);
-        return res.json({status : "Up and Running"});
-    });
-    // router.get('/items',itemApi.getItems);
-    // router.get('/convertArray',itemApi.filterStringsToArrays);
+module.exports = (router) => {
+  router.get('/', (req, res) => {
+    res.status(200)
+    return res.json({ status: 'Up and Running' })
+  })
+  router.get('/items', itemApi.getItems)
+  // router.get('/convertArray',itemApi.filterStringsToArrays);
 }
